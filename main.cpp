81b6302917c09d15d7ce0a54980185ef304762f5
valeriaconte <128838224+valeriaconte@users.noreply.github.com>
2023-05-25 14:36:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+float f(float y){
+    float x=pow(y,2)*cos(y)+1;
+    return x;
+}
+
 int main() {
-   return 0;
+    float a=0, b=0, x, err;
+    do{
+        cout << "inserire estremi\n";
+        cin >> a >> b;
+    } while (f(a)*f(b)>=0);
+    do{
+        x=(a+b)/2;
+        if (f(x)==0){
+            break;
+        }
+        else if (f(a)*f(x)<0){
+            b=x;
+        }
+        else {
+            a=x;
+        }
+        err=abs((b-a)/2);
+    }while (err >= 1e-6);
+    printf("%.4f",x);
+    return 0;
 }
